3206b76bc08218270355afd04168b49a72cadc90
Denis126-dot <denishui126@gmail.com>
2026-05-06 10:26:41 -0400

    add title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -1,13 +1,8 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class TitleScreen here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class TitleScreen extends World
 {
+    Label titleLabel = new Label("Hungry Elephant", 100);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -16,7 +11,15 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        Label titleLabel = new Label("Hungry Elephant", 100);
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
     }
 }
