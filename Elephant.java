2890d7b339054e267001fc1a83cd75b46fe17ee5
Denis126-dot <denishui126@gmail.com>
2026-05-17 00:02:34 -0400

    Update Elephant.java

--- Elephant.java
+++ Elephant.java
@@ -49,12 +49,12 @@ public class Elephant extends Actor
     {
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-5);
+            move(-10);
             facing = "left";
         }
         else if(Greenfoot.isKeyDown("right"))
         {
-            move(5);
+            move(10);
             facing = "right";
         }
         eat();
